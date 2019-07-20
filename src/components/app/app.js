d4af9a7d1a44e7020a192898cae14bc1970deb82
bandey <origbandey@gmail.com>
2019-07-20 20:24:43 +0300

    Add component App

---
+++ src/components/app/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function App() {
+  return (
+    <span>hello</span>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(React.createElement('div', null, 'hello'), document.getElementById('app'));
+import App from './components/app/app.js';
+
+ReactDOM.render(<App />, document.getElementById('app'));
