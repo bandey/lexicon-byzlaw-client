1fa3b27f3b2ad50ab49095cd7d06d70cac481b63
bandey <origbandey@gmail.com>
2019-07-21 00:55:03 +0300

    Add GraphQL query lexiconIntegral for component Lexicon

--- src/components/lexicon/lexicon-data.js
+++ src/components/lexicon/lexicon-data.js
@@ -1,15 +1,31 @@
 import React from 'react';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
 
 import Lexicon from './lexicon-view.js';
 
-const data = [
-  {w: 'one', c: '1'},
-  {w: 'two', c: '2'},
-  {w: 'three', c: '3'},
-];
-
 export default function LexiconData(props) {
   return (
-    <Lexicon data={data} {...props} />
+    <Query
+      query={
+        gql`
+          query {
+            lexiconIntegral { w c }
+          }
+        `
+      }
+    >
+      {({loading, error, data}) => {
+        if (loading) {
+          return <p>Loading...</p>;
+        }
+        if (error) {
+          return <p>Error!</p>;
+        }
+        return (
+          <Lexicon data={data.lexiconIntegral} {...props} />
+        );
+      }}
+    </Query>
   );
 };
