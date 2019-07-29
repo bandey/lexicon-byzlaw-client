bf7a34e16bf46f32aa68548987d40a0bbb079618
bandey <origbandey@gmail.com>
2019-07-29 22:31:23 +0300

    Add react-window to show list in component Lexicon

--- src/components/lexicon/lexicon-view.js
+++ src/components/lexicon/lexicon-view.js
@@ -1,14 +1,31 @@
 import React from 'react';
 
+import { FixedSizeList as List } from 'react-window';
+
 function Lexicon({data}) {
+  const rowHeight = 40;
+
+  function renderRow({index, style}) {
+    const row = data[index];
+    if (!row) {
+      return null;
+    };
+    return (
+      <div style={style}>
+        <span>{row.w} - {row.c}</span>
+      </div>
+    );
+  };
+
   return (
-    <ul>
-      {
-        data.map(({w, c}) => (
-          <li key={w}>{w} - {c}</li>
-        ))
-      }
-    </ul>
+    <List
+      width="100%"
+      height={rowHeight * 10}
+      itemSize={rowHeight}
+      itemCount={data.length}
+    >
+      {renderRow}
+    </List>
   );
 };
 
