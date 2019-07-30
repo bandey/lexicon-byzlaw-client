b61570cc2a2c6e04162785339b079efab6153c91
bandey <origbandey@gmail.com>
2019-07-30 21:42:35 +0300

    Add snapshot test for lexicon-view.js

--- src/components/lexicon/lexicon-view.test.js
+++ src/components/lexicon/lexicon-view.test.js
@@ -1,30 +1,22 @@
 import React from 'react';
-import Enzyme, {shallow} from 'enzyme';
+import Enzyme, {mount} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 Enzyme.configure({adapter: new Adapter()});
 
+import enzymeToJson from 'enzyme-to-json';
+
 import Lexicon from './lexicon-view.js';
 
-it('render content', () => {
+it('renders correct content', () => {
   const data = [
     {w: 'one', c: 1},
     {w: 'two', c: 2},
     {w: 'three', c: 3},
   ];
 
-  const rendered = shallow(<Lexicon data={data} />);
+  const rendered = mount(<Lexicon data={data} />);
 
   // console.log(rendered.debug());
-  /*
-  <List width="100%" height={400} itemSize={40} itemCount={3}>
-    [function renderRow]
-  </List>
-  */
 
-  const list = rendered.find('List');
-  expect(list.exists()).toBeTruthy();
-  expect(list.prop('width')).toEqual('100%');
-  expect(list.prop('height')).toEqual(400);
-  expect(list.prop('itemSize')).toEqual(40);
-  expect(list.prop('itemCount')).toEqual(3);
+  expect(enzymeToJson(rendered)).toMatchSnapshot();
 });
