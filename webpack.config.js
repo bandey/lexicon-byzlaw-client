f2320086eb781b4bebd45827b4d3838e1471d96d
bandey <origbandey@gmail.com>
2019-07-31 00:57:18 +0300

    Add Bootstrap library

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,7 @@ module.exports = {
   plugins: [
     new CopyWebpackPlugin([
       { from: './src/index.html', to: './' },
+      { from: './node_modules/bootstrap/dist/css/bootstrap.min.css', to: './styles/' },
     ], { copyUnmodified: false }),
   ],
   devServer: {
