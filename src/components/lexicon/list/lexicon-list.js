ace3b961b8715e49bb188b1dc17996fe84d925ec
bandey <origbandey@gmail.com>
2019-08-04 17:20:32 +0300

    Add handler for window resize event to LexiconList component

--- src/components/lexicon/list/lexicon-list.js
+++ src/components/lexicon/list/lexicon-list.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {debounce} from 'debounce';
 import {FixedSizeList as List} from 'react-window';
 
 function LexiconList({itemCount, children}) {
   const rowHeight = 40;
-
   const dataHeight = itemCount * rowHeight;
 
-  let listHeight = Math.floor(document.documentElement.clientHeight * 0.7);
-  if (listHeight > dataHeight) {
-    listHeight = dataHeight;
-  }
+  function calcListHeight() {
+    let height = Math.floor(document.documentElement.clientHeight * 0.7);
+    if (height > dataHeight) {
+      height = dataHeight;
+    }
+    return height;
+  };
+
+  const [listHeight, setListHeight] = useState(calcListHeight());
+
+  useEffect(() => {
+    const handleWindowResize = debounce(() => {
+      return setListHeight(calcListHeight());
+    }, 200);
+
+    window.addEventListener('resize', handleWindowResize);
+
+    return function cleanup() {
+      window.removeEventListener('resize', handleWindowResize);
+    };
+  }, [itemCount]);
 
   return (
     <List
