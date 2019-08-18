94d8ef1ef0fd3ce55305308114a54a63fd9ee05f
bandey <origbandey@gmail.com>
2019-08-18 23:33:01 +0300

    Add capitalize for name of chosen operation

--- src/routing/show-product/show-product.js
+++ src/routing/show-product/show-product.js
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom';
 
 import delLastElemFromURL from '../utils/del-last-elem-from-url.js';
 import {BriefPanel} from '../../components/panel/panel.js'
+import {Capitalize} from './show-product-styles.js'
 import DataProvider from '../../data-provider/data-provider.js';
 import {getLexiconOperation} from '../../data-provider/data-provider-queries.js';
 import Lexicon from '../../components/lexicon/lexicon.js';
@@ -17,7 +18,9 @@ function ShowProduct({match}) {
   return (
     <React.Fragment>
       <Link to={delLastElemFromURL(match.url)}>
-        <BriefPanel>{match.params.operation}</BriefPanel>
+        <BriefPanel>
+          <Capitalize>{match.params.operation}</Capitalize>
+        </BriefPanel>
       </Link>
       <DataProvider query={queryLexiconOperation}>
         {Lexicon}
