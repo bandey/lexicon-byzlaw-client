f8a38c3d2fde86b988a559ab46ef562fd0748589
bandey <origbandey@gmail.com>
2020-03-22 23:15:47 +0300

    Fix positions in app-header-styles.js

--- src/components/app/header/app-header-styles.js
+++ src/components/app/header/app-header-styles.js
@@ -2,16 +2,16 @@ import styled from 'styled-components';
 import Col from 'react-bootstrap/Col';
 
 const ColTitle = styled(Col)`
-  padding-top: 1rem;
+  padding-bottom: 1rem;
 `;
 
 const Title = styled.h1`
   margin-top: 0.25rem;
+  margin-bottom: 0;
   font-size: 1.5rem;
 `;
 
 const ColLang = styled(Col)`
-  padding-top: 1rem;
   padding-bottom: 1rem;
 `;
 
