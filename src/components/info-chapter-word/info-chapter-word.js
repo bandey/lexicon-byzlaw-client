d0346c5fac55dbf4fb9b0d8a16d9adb69fcc0873
bandey <origbandey@gmail.com>
2020-11-24 01:07:28 +0300

    Add translations for component InfoChapterWord

--- src/components/info-chapter-word/info-chapter-word.js
+++ src/components/info-chapter-word/info-chapter-word.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {useTranslation} from 'react-i18next';
 
 import {Panel, PanelBody} from '../panel/panel.js';
 import {SpanPolyglot} from '../../styles/polyglot.js';
@@ -8,17 +9,28 @@ function InfoChapterWord({data}) {
     return null;
   }
 
+  const {t} = useTranslation();
+
   return (    
     <Panel border="primary">
       <PanelBody>
         <table>
           <tbody>
-            <tr><td>Словник:</td><td>{data.opus}</td></tr>
-            <tr><td>Статья:</td><td><SpanPolyglot wordWrap>{data.name}</SpanPolyglot></td></tr>
-            <tr><td>Слово:</td><td><SpanPolyglot wordWrap>{data.word}</SpanPolyglot></td></tr>
+            <tr>
+              <td>{t('Source')}:</td>
+              <td>{data.opus}</td>
+            </tr>
+            <tr>
+              <td>{t('Chapter')}:</td>
+              <td><SpanPolyglot wordWrap>{data.name}</SpanPolyglot></td>
+            </tr>
+            <tr>
+              <td>{t('Word')}:</td>
+              <td><SpanPolyglot wordWrap>{data.word}</SpanPolyglot></td>
+            </tr>
           </tbody>
         </table>
-        Количество: {data.count}
+        {t('Quantity')}: {data.count}
       </PanelBody>
     </Panel>
   );
