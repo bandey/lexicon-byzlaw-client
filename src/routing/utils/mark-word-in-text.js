6c19fcd4a67b5a7840e09c662923e12f59110780
bandey <origbandey@gmail.com>
2023-10-28 17:55:44 +0300

    Add whole word flag to markWordInText function

--- src/routing/utils/mark-word-in-text.js
+++ src/routing/utils/mark-word-in-text.js
@@ -3,30 +3,36 @@ import React from 'react';
 import {SpanPolyglot} from '../../styles/polyglot.js';
 
 // Split given text to blocks, where blocks with given word are marked
+// param whole:Boolean 
+//   true - search word embraced by spaces
+//   false - search word as is
 // return [JSX.Element] array of text blocks
-function markWordInText(word, text) {
+function markWordInText(word, text, whole = true) {
   if (!text) return '';
   if (!word) return (<SpanPolyglot whiteSpace>{text}</SpanPolyglot>);
 
   let blocks = [];
 
   const len = word.length;
+  const searched = (whole) ? (' ' + word + ' ') : word;
+  const delta = (whole) ? 1 : 0;
+
   let phrase = '';
   let key = 0;
   let old = 0;
 
-  let pos = text.indexOf(' ' + word + ' ');
+  let pos = text.indexOf(searched);
   while (pos >= 0) {
-    phrase = text.substring(old, pos+1); // beg, end
+    phrase = text.substring(old, pos + delta); // beg, excluded end
     if (phrase) {
       blocks.push(<SpanPolyglot key={++key} whiteSpace>{phrase}</SpanPolyglot>);
     }
 
-    phrase = text.substr(pos+1, len); // beg, len
+    phrase = text.substr(pos + delta, len); // beg, len
     blocks.push(<SpanPolyglot key={++key} whiteSpace marked>{phrase}</SpanPolyglot>);
 
-    old = pos + 1 + len;
-    pos = text.indexOf(' ' + word + ' ', old);
+    old = pos + delta + len;
+    pos = text.indexOf(searched, old);
   }
 
   phrase = text.substring(old);
--- src/routing/utils/mark-word-in-text.test.js
+++ src/routing/utils/mark-word-in-text.test.js
@@ -10,7 +10,7 @@ import markWordInText from './mark-word-in-text.js';
 it('mark word in first position', () => {
   const rendered = mount(
     <React.Fragment>
-      {markWordInText('first', ' first last ')}
+      {markWordInText('first', 'first-last', false)}
     </React.Fragment>
   );
 
@@ -22,7 +22,7 @@ it('mark word in first position', () => {
 it('mark word in middle position', () => {
   const rendered = mount(
     <React.Fragment>
-      {markWordInText('middle', ' first middle last ')}
+      {markWordInText('middle', 'first-middle-last', false)}
     </React.Fragment>
   );
 
@@ -34,19 +34,7 @@ it('mark word in middle position', () => {
 it('mark word in last position', () => {
   const rendered = mount(
     <React.Fragment>
-      {markWordInText('last', ' first last ')}
-    </React.Fragment>
-  );
-
-  // console.log(rendered.debug());
-
-  expect(enzymeToJson(rendered)).toMatchSnapshot();
-});
-
-it('no marking, if word is not found', () => {
-  const rendered = mount(
-    <React.Fragment>
-      {markWordInText('middle', ' first last ')}
+      {markWordInText('last', 'first-last', false)}
     </React.Fragment>
   );
 
@@ -58,7 +46,139 @@ it('no marking, if word is not found', () => {
 it('mark several words', () => {
   const rendered = mount(
     <React.Fragment>
+      {markWordInText('middle', 'first-middlemiddle-last', false)}
+    </React.Fragment>
+  );
+
+  // console.log(rendered.debug());
+
+  expect(enzymeToJson(rendered)).toMatchSnapshot();
+});
+
+it('mark several intersected words', () => {
+  const rendered = mount(
+    <React.Fragment>
+      {markWordInText('axa', 'first-axaxaxa-last', false)}
+    </React.Fragment>
+  );
+
+  // console.log(rendered.debug());
+
+  expect(enzymeToJson(rendered)).toMatchSnapshot();
+});
+
+it('no marking, if word is not found', () => {
+  const rendered = mount(
+    <React.Fragment>
+      {markWordInText('middle', 'first-last', false)}
+    </React.Fragment>
+  );
+
+  // console.log(rendered.debug());
+
+  expect(enzymeToJson(rendered)).toMatchSnapshot();
+});
+
+it('mark whole word in first position', () => {
+  const rendered = mount(
+    <React.Fragment>
+      {markWordInText('first', ' first last ')}
+    </React.Fragment>
+  );
+
+  // console.log(rendered.debug());
+
+  expect(enzymeToJson(rendered)).toMatchSnapshot();
+});
+
+it('mark whole word in middle position', () => {
+  const rendered = mount(
+    <React.Fragment>
+      {markWordInText('middle', ' first middle last ')}
+    </React.Fragment>
+  );
+
+  // console.log(rendered.debug());
+
+  expect(enzymeToJson(rendered)).toMatchSnapshot();
+});
+
+it('mark whole word in last position', () => {
+  const rendered = mount(
+    <React.Fragment>
+      {markWordInText('last', ' first last ')}
+    </React.Fragment>
+  );
+
+  // console.log(rendered.debug());
+
+  expect(enzymeToJson(rendered)).toMatchSnapshot();
+});
+
+it('mark several whole words', () => {
+  const rendered = mount(
+    <React.Fragment>
       {markWordInText('middle', ' first middle middle last ')}
+    </React.Fragment>
+  );
+
+  // console.log(rendered.debug());
+
+  expect(enzymeToJson(rendered)).toMatchSnapshot();
+});
+
+it('no marking, if whole word is not found', () => {
+  const rendered = mount(
+    <React.Fragment>
+      {markWordInText('middle', ' first last ')}
+    </React.Fragment>
+  );
+
+  // console.log(rendered.debug());
+
+  expect(enzymeToJson(rendered)).toMatchSnapshot();
+});
+
+it('no marking, if whole word has no leading space', () => {
+  const rendered = mount(
+    <React.Fragment>
+      {markWordInText('middle', ' first-middle last ')}
+    </React.Fragment>
+  );
+
+  // console.log(rendered.debug());
+
+  expect(enzymeToJson(rendered)).toMatchSnapshot();
+});
+
+it('no marking, if whole word has no tailing space', () => {
+  const rendered = mount(
+    <React.Fragment>
+      {markWordInText('middle', ' first middle-last ')}
+    </React.Fragment>
+  );
+
+  // console.log(rendered.debug());
+
+  expect(enzymeToJson(rendered)).toMatchSnapshot();
+});
+
+it('no marking, if whole word in first position has no leading space', () => {
+  const rendered = mount(
+    <React.Fragment>
+      {markWordInText('first', 'first last ')}
+    </React.Fragment>
+  );
+
+  // console.log(rendered.debug());
+
+  expect(enzymeToJson(rendered)).toMatchSnapshot();
+});
+
+it('no marking, if whole word in last position has no tailing space', () => {
+  const rendered = mount(
+    <React.Fragment>
+      {markWordInText('last', ' first last')}
     </React.Fragment>
   );
 
